16a9677f408f33536bbd65a6da4dc9406ed5cbea
tomasz <tomaszfoster@gmail.com>
2013-06-06 18:26:06 +0000

    created Ruby on Rails application with status updates

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tommyz::Application.config.secret_token = 'c865f3fd3c7ecdec9530cbc4b4649f56d9055433ff362165f669b41b333e83e199c7bb0cf53ad63aa9a7cd1372c9f515fce05d620ea4d3a9a477aa927d9c6532'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Tommyz::Application.config.session_store :cookie_store, key: '_tommyz_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Tommyz::Application.config.session_store :active_record_store
